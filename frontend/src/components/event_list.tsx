87cd8e07a464371f1266bab105d8fb80b13274b6
chengzh2008 <>
2018-11-28 22:09:56 -0800

    finishi the frontend implementation

--- frontend/src/components/event_list.tsx
+++ frontend/src/components/event_list.tsx
@@ -1,22 +1,22 @@
-import {Event} from "../models/event"
-import {EventListItem} from "./event_list_item";
 import * as React from "react";
+import {EventListItem} from "./event_list_item";
+import {Event} from "../model/event";
 
 export interface EventListProps {
-    events: Event[];
+    events: Event[]
+    onEventBooked: (e: Event) => any
 }
 
-
 export class EventList extends React.Component<EventListProps, {}> {
-    render() {
-        const items = this.props.events.map(e =>
-            <EventListItem event={e} />
+    public render() {
+        const items = this.props.events.map(event =>
+            <EventListItem key={event.ID} event={event} onBooked={() => this.props.onEventBooked(event)}/>
         );
 
         return <table className="table">
             <thead>
                 <tr>
-                    <th>Event</th>
+                    <th>Name</th>
                     <th>Where</th>
                     <th colSpan={2}>When (start/end)</th>
                     <th>Actions</th>
@@ -26,6 +26,5 @@ export class EventList extends React.Component<EventListProps, {}> {
                 {items}
             </tbody>
         </table>;
-
     }
 }
